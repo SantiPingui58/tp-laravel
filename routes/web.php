a13595d3bbb1110af547e0914328291f63c8ddb5
SantiPingui58 <santi.falcon@hotmail.com>
2024-06-25 20:42:03 -0300

    Inicio Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StoreController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,17 +19,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/auth/check', [AuthController::class, 'check']);
+Route::get('/auth/user', [AuthController::class, 'user']);
+Route::get('/auth/id', [AuthController::class, 'id']);
+Route::get('/auth/logout', [AuthController::class, 'logout']);
+Route::get('/auth/login', [AuthController::class, 'login']);
+
 
 Route::get('store', [StoreController::class, 'index']);
 Route::get('store/product/{id}',[StoreController::class, 'view']);
-Route::get('store/checkout', [StoreController::class, 'checkout']);
-Route::get('store/cart/add/{id}', [StoreController::class, 'addToCart']);
-Route::get('store/cart/remove/{id}', [StoreController::class, 'removeFromCart']);
+Route::get('store/checkout', [StoreController::class, 'checkout'])->middleware('auth');
+Route::get('store/cart/add/{id}', [StoreController::class, 'addToCart'])->middleware('auth');
+Route::get('store/cart/remove/{id}', [StoreController::class, 'removeFromCart'])->middleware('auth');
 
-Route::get('store/admin', [StoreController::class, 'admin']);
-Route::get('store/admin/new', [StoreController::class, 'new']);
-Route::get('store/admin/product/{id}/edit',[StoreController::class, 'edit']);
-Route::post('store/admin/product/{id}/update', [StoreController::class, 'update']);
-Route::post('store/admin/product/{id}/delete', [StoreController::class, 'delete']);
-Route::post('store/admin/product/create', [StoreController::class, 'create']);
+Route::get('store/admin', [StoreController::class, 'admin'])->middleware('auth');
+Route::get('store/admin/new', [StoreController::class, 'new'])->middleware('auth');
+Route::get('store/admin/product/{id}/edit',[StoreController::class, 'edit'])->middleware('auth');
+Route::post('store/admin/product/{id}/update', [StoreController::class, 'update'])->middleware('auth');
+Route::post('store/admin/product/{id}/delete', [StoreController::class, 'delete'])->middleware('auth');
+Route::post('store/admin/product/create', [StoreController::class, 'create'])->middleware('auth');
 
